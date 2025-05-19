fc9073031186301b3bcdfa66d2e8b26840b783a8
Srikanth-Aryasomayajula <venkatasrikantharyasomayajula@gmail.com>
2025-05-19 12:58:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,9 @@ function boldWordsInTable(tbody, keywords) {
 
 function getBoldWords() {
   return [
-    "Nominativ", "Akkusativ", "Dativ", "Genetiv", "Maskulin", "Feminin", "Neuter",
+    "I",	"ich", "you", "you (inf.)",	"du", "he/she/it",	"er/sie/se", "we", 
+	"wir", "you (pl.,inf.)",	"ihr", "you (form.)/they",	"Sie/sie",
+	"Nominativ", "Akkusativ", "Dativ", "Genetiv", "Maskulin", "Feminin", "Neuter",
     "Plural", "Remarks", "Type", "Case", "(O-FUDGE-bis)", "(MAN-VS-BAGZ)", "Wechsel",
     "(displacement vs position)", "(UÜ VIZ. HAAN)", "Präsenz", "Singular", "MV - Singular",
     "MV - Plural", "NS - Singular", "NS - Plural", "NS mit MV - Sin.", "NS mit MV - Pl.",
@@ -297,9 +299,13 @@ function getMergeConfigsGram() {
     { text: ["Dativ (Position)"], matchPartial: false, direction: "row", span: 2, style: styleCenter() },
     { text: ["Präpostionen"], matchPartial: false, direction: "row", span: 3, style: styleCenter() },
     { text: ["Ausnahme:   zu Hause = at home"], matchPartial: true, direction: "row", span: 3, style: styleCenter() },
+	{ text: ["werden + Partizip II", "wurden + Partizip II", "sein/haben + Partizip II + worden", "war + Part. II + worden", "werden + Part. II + werden", "werden + Partizip II + worden + sein"], matchPartial: false, direction: "row", span: 3, style: { fontWeight: "bold", textAlign: "left", verticalAlign: "middle" }},
     { text: ["MV: Modal Verb"], matchPartial: true, direction: "col", span: 3, style: styleCenter() },
     { text: ["Wechsel Verben (Wohin oder wo?)", "Passiv Satz", "N-Deklanation"], matchPartial: false, direction: "row", span: 4, style: styleCenter() },
-    { text: ["Konjuktiv II (I would come if…. Form) Conjugation"], matchPartial: false, direction: "row", span: 8, style: styleCenter() },
+    { text: ["Personal Pronomen", "Adjektiv Deklanation (mit bestimmten artikel der/die/das)", "Adjektiv Deklanation (mit unbestimmten artikel ein/eine/ein)", "Adjektiv Deklanation (ohne artikel)", "Relativpronomen", "Relativ Satz (Just like 'which', 'who', etc. in English)"], matchPartial: false, direction: "row", span: 5, style: styleCenter() },
+	{ text: ["Bestimmtes / Unbestimmtes Wort", "Examples of Adjektiv Deklanation", "Personal Pronomen Adjektiv Deklanation"], matchPartial: false, direction: "row", span: 6, style: styleCenter() },
+	{ text: ["Hinweis: zwei, drei, vier"], matchPartial: true, direction: "row", span: 6, style: styleCenter() },
+	{ text: ["Konjuktiv II (I would come if…. Form) Conjugation"], matchPartial: false, direction: "row", span: 8, style: styleCenter() },
     { text: [`Ich hätte gern`], matchPartial: true, direction: "row", span: 11, style: styleCenter() },
     { text: [`Note: "möchten"`], matchPartial: true, direction: "row", span: 11, style: styleCenter() },
     { text: [`Verb Kojuktion (`], matchPartial: true, direction: "row", span: 11, style: styleCenter() }
